28acc9590bd55afecb48bb4e9b573e18095d5d3e
asya-ndao <sene.asya@gmail.com>
2025-02-05 18:16:46 +0000

    practice Typescript/Tailwindcss

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../type/todos";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Learn Typescript.",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Do a project.",
+        completed: false,
+    },
+    {
+        id:3,
+        title: "Push projet to github.",
+        completed: false,
+    }
+]
